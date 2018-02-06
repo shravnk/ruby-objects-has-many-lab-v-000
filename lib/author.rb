c75308b9551af8d9cdcefc2564dacd8d2eabcfe9
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 18:00:36 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << posts
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_song_by_name(name)
+    post = Post.new(name)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.song_count
+    @@post_count
+  end
+end
